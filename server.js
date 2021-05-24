99e02a28015e38cc2c7d8968d5ddfe8842dcfd3b
David Valderrama <dvalderrama08@gmail.com>
2021-05-24 09:38:40 -0700

    Added api routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,5 +13,21 @@ app.use("/js", express.static(__dirname + '/assets/js'));
 // Basic route that sends the user first to the AJAX Page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+// API calls
+app.get('/api/notes', (req, res) => {
+    fs.readFile(`${__dirname}/db/db.json`, 'utf-8', (err, data) => {
+        if (err) throw err;
+        
+        res.json(JSON.parse(data));
+    });
+});
+app.post('/api/notes', (req, res) => {
+
+});
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(`Delete received for ID: ${id}`);
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
